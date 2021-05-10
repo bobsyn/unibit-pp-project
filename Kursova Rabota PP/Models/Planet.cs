56eaf5d1340e7a2c5a58e43edc9b1d2018e272ce
bobsyn <83972321+bobsyn@users.noreply.github.com>
2021-05-10 22:59:44 +0300

    Add files via upload

---
+++ Kursova Rabota PP/Models/Planet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kursova_Rabota_PP.Models
+{
+    public class Planet
+    {
+        private Star star;
+        public string PlanetName { get; set; }
+        public string PlanetType { get; set; }
+        public bool isHabitable { get; set; }
+        public List<Moon> Moons;
+        public Planet(string name, string type, bool ishabitable)
+        {
+            PlanetName = name;
+            PlanetType = type;
+            isHabitable = ishabitable;
+            this.Moons = new List<Moon>();
+        }
+        
+    }
+}
+
---
+++ galaxyApp/Modаls/Moon.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalaxyCatalog.Modals
+{
+    public class Moon: ICountList
+    {
+        // Полета
+
+        private string moonName;
+        static int instances = 0;
+
+        // Конструктори
+
+        public Moon(string moonName)
+        {
+            this.moonName = moonName;
+
+            instances++;
+        }
+
+        // Свойства
+
+        public string MoonName {
+            get { return this.moonName;  }
+        }
+
+        // Методи
+
+        public static int GetActiveInstances()
+        {
+            return instances;
+        }
+        int ICountList.GetActiveInstances()
+        {
+            return Moon.GetActiveInstances();
+        }
+
+        public string GetInnerList(string view)
+        {
+            return "";
+        }
+
+
+        public override string ToString()
+        {
+            return this.moonName;
+        }
+
+    }
+}
